b3d200a8c77d1eb4fd76b763e74f600ec43b0639
syedabdullahali <syedabdullah380@gmail.com>
2023-03-17 23:36:21 +0530

    added new

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".menue-icon").addEventListener('click',()=>{
+  document.querySelector('.menu-bar').classList.toggle("menu-bar-active")
+  document.querySelector('.navbar-social').classList.toggle("navbar-social-active")
+})
+
